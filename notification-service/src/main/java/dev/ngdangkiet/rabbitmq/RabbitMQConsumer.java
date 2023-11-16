c9f4636ffe2cbe75b851bd49448bb5007e13b1aa
trong.le-duc <trong.le-duc@banvien.com.vn>
2023-11-16 18:01:26 +0700

    init send email

--- notification-service/src/main/java/dev/ngdangkiet/rabbitmq/RabbitMQConsumer.java
+++ notification-service/src/main/java/dev/ngdangkiet/rabbitmq/RabbitMQConsumer.java
@@ -2,11 +2,15 @@ package dev.ngdangkiet.rabbitmq;
 
 import dev.ngdangkiet.constant.RabbitMQConstant;
 import dev.ngdangkiet.domain.JsonMessage;
+import dev.ngdangkiet.service.EmailService;
 import dev.ngdangkiet.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 /**
  * @author ngdangkiet
@@ -20,9 +24,16 @@ public class RabbitMQConsumer {
 
     private final NotificationService notificationService;
 
+    @Autowired
+    private EmailService emailService;
+
     @RabbitListener(queues = {RabbitMQConstant.Notification.QUEUE})
-    public void receiveWelcomeNotification(JsonMessage message) {
+    public void receiveNotification(JsonMessage message) {
         log.info(String.format("Received message -> %s", message.toString()));
-        notificationService.receiveWelcomeNotification(message);
+        if (Objects.nonNull(message.getEmailTemplate())) {
+            notificationService.receiveEmailActiveAccount(message);
+        } else {
+            notificationService.receiveWelcomeNotification(message);
+        }
     }
 }
