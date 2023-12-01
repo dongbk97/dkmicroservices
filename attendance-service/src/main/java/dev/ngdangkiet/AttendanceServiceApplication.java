40ce6b00c90664b6eee474a571ea70526093af87
kiet.nguyen-dang <kietnguyen17052001@gmail.com>
2023-12-01 17:07:42 +0700

    init attendance service

---
+++ attendance-service/src/main/java/dev/ngdangkiet/AttendanceServiceApplication.java
@@ -0,0 +1,17 @@
+package dev.ngdangkiet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ngdangkiet
+ * @since 12/1/2023
+ */
+
+@SpringBootApplication
+public class AttendanceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttendanceServiceApplication.class, args);
+    }
+}
